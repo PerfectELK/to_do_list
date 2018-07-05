eb2983c49afec2943e41ed0b7fbd9e2b4a73b149
Losev Andrey <Losikoban@Gmail.com>
2018-07-06 00:35:26 +0300

    resolved

---
+++ js/script.js
@@ -0,0 +1,72 @@
+
+var INDEX_STORAGE = "TO DO LIST";
+console.log(localStorage);
+
+var vue = new Vue({
+    el:'#app',
+    data:{
+        tasks: JSON.parse(localStorage.getItem(INDEX_STORAGE)) || []
+    },
+    methods:{
+        addTask: function(){
+            if(this.task != "") {
+                var obj = {
+                    id: this.tasks.length + 1,
+                    done: false,
+                    task: this.task
+                };
+                this.tasks.push(obj);
+                var object = JSON.stringify(this.tasks);
+                localStorage.setItem(INDEX_STORAGE, object);
+            }
+        },
+        removeTask: function(identificator){
+            for(var i = 0; i < this.tasks.length; i++){
+                var task_id = this.tasks[i].id;
+                if(task_id == identificator){
+                    this.tasks.splice(i,1);
+                    var object = JSON.stringify(this.tasks);
+                    localStorage.setItem(INDEX_STORAGE, object);
+                }
+            }
+        },
+        syncTask(identificator){
+            console.log(identificator);
+            var object = JSON.stringify(this.tasks);
+            localStorage.setItem(INDEX_STORAGE, object);
+        },
+        renderAllTask(){
+            this.tasks = JSON.parse(localStorage.getItem(INDEX_STORAGE)) || [];
+        },
+        renderResolvedTask(){
+            this.tasks = JSON.parse(localStorage.getItem(INDEX_STORAGE)) || [];
+            var arr = [];
+            for(var i = 0; i < this.tasks.length; i++) {
+                var task_done = this.tasks[i].done;
+                if (task_done === true) {
+                    arr.push({
+                        id: this.tasks[i].id,
+                        done: this.tasks[i].done,
+                        task: this.tasks[i].task
+                    });
+                }
+            }
+            this.tasks = arr;
+        },
+        renderNotResolvedTask(){
+            this.tasks = JSON.parse(localStorage.getItem(INDEX_STORAGE)) || [];
+            var arr = [];
+            for(var i = 0; i < this.tasks.length; i++) {
+                var task_done = this.tasks[i].done;
+                if (task_done === false) {
+                    arr.push({
+                        id: this.tasks[i].id,
+                        done: this.tasks[i].done,
+                        task: this.tasks[i].task
+                    });
+                }
+            }
+            this.tasks = arr;
+        }
+    }
+});
